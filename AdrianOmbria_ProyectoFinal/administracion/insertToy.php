ee23626752ba6819b170396c7e1d8b43439504a6
Adrian Ombria Domingo <adrian.ombria41@gmail.com>
2021-11-26 11:05:49 +0100

    icono pestaña

--- AdrianOmbria_ProyectoFinal/administracion/insertToy.php
+++ AdrianOmbria_ProyectoFinal/administracion/insertToy.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Oblectatio - Perfil</title>
+    <title>Oblectatio - Panel Admin</title>
+    <link rel="icon" href="../img/logo.png">
     <link rel="stylesheet" href="css/singup.css">
 </head>
 <body>
